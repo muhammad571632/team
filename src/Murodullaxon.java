0f7b4ed61850cadd261d0762a38be77d6ccffefa
sdlxnvv <murodsadulloxonov@yandex.ru>
2025-05-14 23:44:02 +0500

    murod ozgartirish kiritdi

--- src/Murodullaxon.java
+++ src/Murodullaxon.java
@@ -1,5 +1,6 @@
 public class Murodullaxon {
     public static void main(String[] args) {
         System.out.println("brodar Assalomu laaykum boladi ");
+        System.out.println("hop brodar ");
     }
 }
